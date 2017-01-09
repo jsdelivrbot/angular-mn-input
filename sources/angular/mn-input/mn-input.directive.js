c1ddf14976a80ea7013214993db9474a435bcc2f
Darlan Mendonça <darlanmendonca@gmail.com>
2017-01-09 11:19:37 -0200

    update mn-input and remove angular-messages

--- sources/angular/mn-input/mn-input.directive.js
+++ sources/angular/mn-input/mn-input.directive.js
@@ -14,35 +14,35 @@ function mnInputDirective($compile) {
     input.attr('ng-model', attributes.ngModel)
     $compile(input)(scope)
 
-    const formName = element[0].closest('form').getAttribute('name')
+    // const formName = element[0].closest('form').getAttribute('name')
 
-    if (attributes.name && formName) {
-      const messagesRule = 'formName.$submitted && formName.inputName.$error'
-        .replace(/formName/g, formName)
-        .replace(/inputName/g, attributes.name)
+    // if (attributes.name && formName) {
+    //   const messagesRule = 'formName.$submitted && formName.inputName.$error'
+    //     .replace(/formName/g, formName)
+    //     .replace(/inputName/g, attributes.name)
 
-      const validations = [
-        {
-          name: 'required',
-          text: 'is required',
-        },
-        {
-          name: 'email',
-          text: 'is invalid',
-        },
-      ]
+    //   const validations = [
+    //     {
+    //       name: 'required',
+    //       text: 'is required',
+    //     },
+    //     {
+    //       name: 'email',
+    //       text: 'is invalid',
+    //     },
+    //   ]
 
-      const label = element[0].querySelector('label')
-      label.setAttribute('ng-messages', messagesRule)
-      validations.forEach(setMessage)
-      $compile(label)(scope)
+    //   const label = element[0].querySelector('label')
+    //   label.setAttribute('ng-messages', messagesRule)
+    //   validations.forEach(setMessage)
+    //   $compile(label)(scope)
 
-      function setMessage(validation) {
-        const message = document.createElement('span')
-        message.setAttribute('ng-message', validation.name)
-        message.textContent = ` ${validation.text}`
-        label.appendChild(message)
-      }
-    }
+    //   function setMessage(validation) {
+    //     const message = document.createElement('span')
+    //     message.setAttribute('ng-message', validation.name)
+    //     message.textContent = ` ${validation.text}`
+    //     label.appendChild(message)
+    //   }
+    // }
   }
 }
