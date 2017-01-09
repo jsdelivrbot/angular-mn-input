a28272c906811195bc51b9ecd93b12a0d3b2b7b1
Darlan Mendonça <darlanmendonca@gmail.com>
2017-01-09 14:29:33 -0200

    fix to assign a value based on ngModel

--- sources/angular/mn-input/mn-input.directive.js
+++ sources/angular/mn-input/mn-input.directive.js
@@ -2,7 +2,7 @@ angular
   .module('mn-input')
   .directive('mnInput', mnInputDirective)
 
-function mnInputDirective($compile) {
+function mnInputDirective($compile, $parse) {
   return {
     restrict: 'E',
     link,
@@ -11,38 +11,8 @@ function mnInputDirective($compile) {
 
   function link(scope, element, attributes) {
     const input = element.find('input')
+    element[0].value = $parse(attributes.ngModel)(scope)
     input.attr('ng-model', attributes.ngModel)
     $compile(input)(scope)
-
-    // const formName = element[0].closest('form').getAttribute('name')
-
-    // if (attributes.name && formName) {
-    //   const messagesRule = 'formName.$submitted && formName.inputName.$error'
-    //     .replace(/formName/g, formName)
-    //     .replace(/inputName/g, attributes.name)
-
-    //   const validations = [
-    //     {
-    //       name: 'required',
-    //       text: 'is required',
-    //     },
-    //     {
-    //       name: 'email',
-    //       text: 'is invalid',
-    //     },
-    //   ]
-
-    //   const label = element[0].querySelector('label')
-    //   label.setAttribute('ng-messages', messagesRule)
-    //   validations.forEach(setMessage)
-    //   $compile(label)(scope)
-
-    //   function setMessage(validation) {
-    //     const message = document.createElement('span')
-    //     message.setAttribute('ng-message', validation.name)
-    //     message.textContent = ` ${validation.text}`
-    //     label.appendChild(message)
-    //   }
-    // }
   }
 }
